85517f456f9d9f9a117c8efe1de9efd0476d7fd4
Kohsuke Kawaguchi <kk@kohsuke.org>
2011-02-01 09:15:03 -0800

    added cleanup code to fix up an early failed import.

--- src/main/java/BulkImport.java
+++ src/main/java/BulkImport.java
@@ -2,16 +2,43 @@ import org.jenkinsci.account.Application;
 import org.jenkinsci.account.WebAppMain;
 
 import javax.naming.NameAlreadyBoundException;
+import javax.naming.NamingEnumeration;
+import javax.naming.directory.SearchControls;
+import javax.naming.directory.SearchResult;
+import javax.naming.ldap.LdapContext;
 import java.io.File;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * @author Kohsuke Kawaguchi
  */
 public class BulkImport {
     public static void main(String[] args) throws Exception {
-        File dir = new File(args[0]);
-
         Application app = new WebAppMain().createApplication();
+
+        Set<String> names = new HashSet<String>();
+        SearchControls cons = new SearchControls();
+        cons.setReturningAttributes(new String[]{"cn"});
+        NamingEnumeration<SearchResult> e = app.connect().search("ou=people,dc=jenkins-ci,dc=org", "(objectClass=inetOrgPerson)", cons);
+        while (e.hasMore()) {
+            SearchResult r = e.nextElement();
+            String cn = (String)r.getAttributes().get("cn").get();
+            names.add(cn);
+        }
+
+        {// clean up bogus entries
+            LdapContext ldap = app.connect();
+            for (String name : names) {
+                if (!name.toLowerCase().equals(name) || name.contains("@")) {
+                    // delete this
+                    System.out.println("Deleting "+name);
+                    ldap.destroySubcontext("cn=" + name + ",ou=people,dc=jenkins-ci,dc=org");
+                }
+            }
+        }
+
+        File dir = new File(args[0]);
         System.out.println("Listing up "+dir);
         for (File f : dir.listFiles()) {
             if (f.exists() && !f.isDirectory()) {
@@ -19,9 +46,11 @@ public class BulkImport {
                 System.out.println(name);
                 if (name.contains("@"))  continue;   // invalid
 
+                if (names.contains(name))   continue;   // already imported
+
                 try {
                     app.createRecord(name,f.getName(),"-",f.getName()+"@java.net");
-                } catch (NameAlreadyBoundException e) {
+                } catch (NameAlreadyBoundException x) {
                     // already registered. move on
                 }
             }
