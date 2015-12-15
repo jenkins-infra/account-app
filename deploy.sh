34e1c87ea977df7acf1f46f1ecf9e6db75e4949d
Kohsuke Kawaguchi <kk@kohsuke.org>
2015-12-15 10:42:29 -0800

    restart isn't working

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,6 @@
 # deploy to the production server
 mvn clean package
 scp target/accountapp.war accountapp@cucumber:~
-ssh cucumber sudo /etc/init.d/accountapp restart
+ssh cucumber sudo /etc/init.d/accountapp stop
+sleep 3
+ssh cucumber sudo /etc/init.d/accountapp start
