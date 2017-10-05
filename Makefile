f408f040df6f347d4a551ab14f10a4da817794c2
olblak <olivier@vernin.me>
2017-10-05 21:41:25 +0200

    Improve tests environment
        * voteGenerator.py generate fake election date
        * Define Make rule to deploy an accountapp with open election and
          another one with close election

--- Makefile
+++ Makefile
@@ -8,9 +8,17 @@ build:
 
 clean:
 	docker run --rm -v $(current_dir):/opt/accountapp/ -u $(current_user):$(current_user) -w /opt/accountapp --entrypoint ./gradlew openjdk:8-jdk clean
+	rm -Rf ./fake_data
 
-sandbox:
-	docker-compose up --build sandbox
+generate_data:
+	@if [ ! -d fake_data ]; then mkdir fake_data ;fi
+	@./voteGenerator.py
 
-prod:
-	docker-compose up --build prod
+open_election: generate_data
+	docker-compose up --build open_election
+
+close_election: generate_data
+	docker-compose up --build close_election
+
+local:
+	docker-compose up --build local
---
+++ voteGenerator.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+#Python 2.7.13
+import random
+import md5
+import uuid
+import sys
+import os
+
+# This script generate fake election data for testing purposes.
+
+elections = [
+        '19931123',
+        '20031124',
+        '20131123',
+        ]
+
+candidates = [
+        'bob',
+        'alice',
+        'john',
+        'paul',
+        'jack',
+        'tom'
+        ]
+
+for election in elections:
+    dirname = os.getcwd() + "/" + 'fake_data' + "/" + election + "/"
+    directory = os.path.dirname(dirname)
+
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    for i in range(100):
+        vote=[]
+        c = list(candidates)
+        file = open( os.path.join( dirname, uuid.uuid4().hex),"w" )
+        while (len(c) > 0):
+            secure_random = random.SystemRandom()
+            selected = secure_random.choice(c)
+            c.remove(selected)
+            vote.append(selected)
+            file.write(selected + "\n")
+        file.close
+
+print "Fake data created"
+
