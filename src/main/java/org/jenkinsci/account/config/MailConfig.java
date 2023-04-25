ea806096e96e6d959e898c39b3e75157e2b56e03
Hervé Le Meur <91831478+lemeurherve@users.noreply.github.com>
2023-04-25 17:17:58 +0200

    feat: allow configuring the sender email address (#264)

--- src/main/java/org/jenkinsci/account/config/MailConfig.java
+++ src/main/java/org/jenkinsci/account/config/MailConfig.java
@@ -3,13 +3,15 @@ package org.jenkinsci.account.config;
 public class MailConfig {
 
     private final String smtpServer;
+    private final String smtpSender;
     private final String smtpUser;
     private final String smtpPassword;
     private final boolean smtpAuth;
     private final int smtpPort;
 
-    public MailConfig(String smtpServer, int smtpPort, String smtpUser, String smtpPassword, boolean smtpAuth) {
+    public MailConfig(String smtpServer, String smtpSender, int smtpPort, String smtpUser, String smtpPassword, boolean smtpAuth) {
         this.smtpServer = smtpServer;
+        this.smtpSender = smtpSender;
         this.smtpUser = smtpUser;
         this.smtpPassword = smtpPassword;
         this.smtpAuth = smtpAuth;
@@ -18,6 +20,10 @@ public class MailConfig {
 
     public String getSmtpServer() {
         return smtpServer;
+    }
+
+    public String getSmtpSender() {
+        return smtpSender;
     }
 
     public String getSmtpUser() {
