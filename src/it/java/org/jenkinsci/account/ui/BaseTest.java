98597d25ed4c31fdd701f400d2b96fa3757d6e16
Hervé Le Meur <lemeurherve.jenkins@gmail.com>
2026-07-13 18:43:07 +0200

    missing import

--- src/it/java/org/jenkinsci/account/ui/BaseTest.java
+++ src/it/java/org/jenkinsci/account/ui/BaseTest.java
@@ -32,6 +32,7 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.WebDriver;
 
 @ExtendWith(BaseTest.ScreenShotOnFailedTestExtension.class)
 public class BaseTest {
