8a8b0c490db0e47af186843219e3dd4924688bd6
Hervé Le Meur <lemeurherve.jenkins@gmail.com>
2026-07-13 19:54:44 +0200

    fixup

--- src/it/java/org/jenkinsci/account/ui/BaseTest.java
+++ src/it/java/org/jenkinsci/account/ui/BaseTest.java
@@ -32,7 +32,6 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.WebDriverManager;
 
 @ExtendWith(BaseTest.ScreenShotOnFailedTestExtension.class)
 public class BaseTest {
